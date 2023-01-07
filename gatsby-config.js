d9a72034bde7df9392ca9069cd325c5213793c0b
Troy Lin <troy@Troys-MacBook-Pro.local>
2023-01-07 16:56:35 +1100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `troysite`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
