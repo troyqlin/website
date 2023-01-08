5018607efd12d9620309b72a1101e6d2e634738c
Troy Lin <troy@Troys-MacBook-Pro.local>
2023-01-08 19:00:13 +1100

    Followed instructions to set up seo settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,11 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `troysite`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `troy lin`,
+    description: `troy's site, home to future personal projects and information.`,
+    twitterUsername: `@troyqlin`,
+    image: `/charlieBrown.png`,
+    siteUrl: `https://www.troylin.com.au`,
   },
   plugins: [],
 }
