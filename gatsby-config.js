b8f9541b31168a2abd1af90099986bda20445ad4
Troy Lin <troy@Troys-MacBook-Pro.local>
2023-01-09 15:40:33 +1100

    Attempted to optimise site seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,11 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `troy lin`,
-    description: `troy's site, home to future personal projects and information.`,
-    twitterUsername: `@troyqlin`,
-    image: `/charlieBrown.png`,
-    siteUrl: `https://www.troylin.com.au`,
+    title: "troy lin",
+    description: "troy's site, home to future personal projects and information.",
+    twitterUsername: "@troyqlin",
+    image: "/charlieBrown.jpg",
+    siteUrl: "https://www.troylin.com.au",
   },
   plugins: [],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../pages/layout'
+import { Seo } from "../components/seo"
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -12,7 +13,11 @@ const IndexPage = () => {
 }
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+// export const Head = () => <title>Home Page</title>
+
+export const Head = () => (
+  <Seo title="Home Page" />
+)
 
 // Step 3: Export your component
 export default IndexPage
