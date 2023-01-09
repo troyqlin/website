6d435350b2f90fefa050a6f330cdbbf599d4ea52
Troy Lin <troy@Troys-MacBook-Pro.local>
2023-01-09 16:04:29 +1100

    added meta data for the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../pages/layout'
+import { Seo } from '../components/seo'
 
 // Step 2: Define your component
 const AboutPage = () => {
@@ -11,7 +12,11 @@ const AboutPage = () => {
   )
 }
 
-export const Head = () => <title>About Me</title>
+// export const Head = () => <title>About Me</title>
+
+export const Head = () => (
+  <Seo title="About Page" description="Troy's about page. Here, you'll learn how cool I am."/>
+)
 
 // Step 3: Export your component
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../pages/layout'
-import { Seo } from "../components/seo"
+import { Seo } from '../components/seo'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>That's right. I MADE THIS WEBSITE HEHHEHEHEHEH.</p>
     </Layout>
   )
 }
