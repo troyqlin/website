f3ba378795a7c455bc22a9bad03aeec256a44d6e
Troy Lin <troy@Troys-MacBook-Pro.local>
2023-01-15 15:01:14 +1100

    Previous one didn't work. Gonna try use general configuration with the sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,53 +18,6 @@ module.exports = {
         policy: [{userAgent: '*', allow: '/'}]
       }
     },
-    {
-      resolve: "gatsby-plugin-sitemap",
-      options: {
-        query: `
-        {
-          allSitePage {
-            nodes {
-              path
-            }
-          }
-          allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
-            nodes {
-              ... on WpPost {
-                uri
-                modifiedGmt
-              }
-              ... on WpPage {
-                uri
-                modifiedGmt
-              }
-            }
-          }
-        }
-      `,
-        resolveSiteUrl: () => "https://www.troylin.com.au",
-        resolvePages: ({
-          allSitePage: { nodes: allPages },
-          allWpContentNode: { nodes: allWpNodes },
-        }) => {
-          const wpNodeMap = allWpNodes.reduce((acc, node) => {
-            const { uri } = node
-            acc[uri] = node
-
-            return acc
-          }, {})
-
-          return allPages.map(page => {
-            return { ...page, ...wpNodeMap[page.path] }
-          })
-        },
-        serialize: ({ path, modifiedGmt }) => {
-          return {
-            url: path,
-            lastmod: modifiedGmt,
-          }
-        },
-      },
-    },
+    `gatsby-plugin-sitemap`
   ],
 }
