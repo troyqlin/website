cbe3a426fbabd2e61a611f55d6e3351af70dafaf
Troy Lin <troy@Troys-MacBook-Pro.local>
2023-01-16 20:01:18 +1100

    Forgot to commit a few files

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: "troy lin",
+    title: "troy's website",
     description: "troy's site, home to future personal projects and information.",
     twitterUsername: "@troyqlin",
     image: "/charlieBrown.jpg",
@@ -21,5 +21,12 @@ module.exports = {
     "gatsby-plugin-sitemap",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
